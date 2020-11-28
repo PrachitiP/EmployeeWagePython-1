6bca161bc2630effac8146759b8ac8fea6d41bbb
saurav004 <logtosaurav@gmail.com>
2020-11-29 01:58:22 +0530

    Added:Calculating Salary of Employee for one Month based on Attendance

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -14,18 +14,24 @@ class EmployeeWage:
     def employee_attendance(cls):
         attendance = random.randint(0, 2)
         if attendance == EmployeeWage.FULL_TIME:
-            print("Employee is Full time Present")
+            print("Employee is present for : Full Time")
             EmployeeWage.hours_per_day = EmployeeWage.full_time_employee_hours
         elif attendance == EmployeeWage.PART_TIME:
-            print("Employee is present for part time")
+            print("Employee is present for : Part time")
             EmployeeWage.hours_per_day = EmployeeWage.part_time_employee_hours
         else:
             print("Employee is Absent")
-            EmployeeWage.hours_per_day = 0, cls
+            EmployeeWage.hours_per_day = 0
 
 
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
-    EmployeeWage.employee_attendance()
-    EmployeeWage.employee_daily_wage = EmployeeWage.wage_per_hour * EmployeeWage.hours_per_day
-    print(f"Employee's Salary for the day is {EmployeeWage.employee_daily_wage}")
+    employee_wage_for_a_month = 0
+    for day in range(1, 21):
+        print(f"day {day} : ")
+        EmployeeWage.employee_attendance()
+        EmployeeWage.employee_daily_wage = EmployeeWage.wage_per_hour * EmployeeWage.hours_per_day
+        print(f"Employee's salary for day {day} is : {EmployeeWage.employee_daily_wage}")
+        employee_wage_for_a_month = employee_wage_for_a_month + EmployeeWage.employee_daily_wage
+    else:
+        print(f"\nEmployee's Salary for the Entire Month is: {employee_wage_for_a_month}")
