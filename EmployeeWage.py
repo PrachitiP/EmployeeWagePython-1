0f677710ccfe7240439ddd5d1dbab22e5dc67ed9
saurav004 <logtosaurav@gmail.com>
2020-11-28 14:16:49 +0530

    Added:Employee attendance is checked

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,18 @@
-print("Welcome to Employee Wage Computation Program")
+import random
+
+
+class EmployeeWage:
+
+    def employeeattendance(self):
+        attendance = random.randint(0, 1)
+        if attendance == 0:
+            print("Present")
+        else:
+            print("Absent")
+        return attendance
+
+
+if __name__ == '__main__':
+    print("Welcome to Employee Wage Computation Program")
+    employee = EmployeeWage()
+    isPresent = employee.employeeattendance()
