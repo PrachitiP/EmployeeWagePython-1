69afe33cbf24ba4d7a5443428f6368f1105cc375
saurav004 <logtosaurav@gmail.com>
2020-11-29 23:03:13 +0530

    Added:Switch Case using Dictionary

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -12,20 +12,22 @@ class EmployeeWage:
 
     @classmethod
     def employee_attendance(cls):
-        attendance = random.randint(0, 2)
-        if attendance == EmployeeWage.FULL_TIME:
-            print("Employee is Full time Present")
-            EmployeeWage.hours_per_day = EmployeeWage.full_time_employee_hours
-        elif attendance == EmployeeWage.PART_TIME:
-            print("Employee is present for part time")
-            EmployeeWage.hours_per_day = EmployeeWage.part_time_employee_hours
-        else:
-            print("Employee is Absent")
-            EmployeeWage.hours_per_day = 0, cls
+        presence = random.randint(0, 2)
+        return presence
+
+    @classmethod
+    def employee_work_hours(cls, attendance_of_employee):
+        switch = {
+            0: EmployeeWage.full_time_employee_hours,
+            1: EmployeeWage.part_time_employee_hours,
+            2: 0
+        }
+        EmployeeWage.hours_per_day = switch.get(attendance_of_employee)
+        print(EmployeeWage.hours_per_day)
 
 
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
-    EmployeeWage.employee_attendance()
+    EmployeeWage.employee_work_hours(EmployeeWage.employee_attendance())
     EmployeeWage.employee_daily_wage = EmployeeWage.wage_per_hour * EmployeeWage.hours_per_day
     print(f"Employee's Salary for the day is {EmployeeWage.employee_daily_wage}")
